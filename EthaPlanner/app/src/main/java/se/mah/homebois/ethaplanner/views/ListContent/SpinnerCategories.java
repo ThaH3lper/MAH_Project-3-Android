a978963d09f157348e0bf6bd0089bd483ba5a9d1
Simon Bothén <caresilabs@gmail.com>
2016-10-23 15:27:47 +0200

    Spinner fix

--- EthaPlanner/app/src/main/java/se/mah/homebois/ethaplanner/views/ListContent/SpinnerCategories.java
+++ EthaPlanner/app/src/main/java/se/mah/homebois/ethaplanner/views/ListContent/SpinnerCategories.java
@@ -1,17 +1,14 @@
 package se.mah.homebois.ethaplanner.views.ListContent;
 
-import se.mah.homebois.ethaplanner.R;
-
 /**
  * Created by Tim on 2016-10-23.
  */
 public class SpinnerCategories {
     public static SpinnerItem[] spinnerItems = new SpinnerItem[]{
             new SpinnerItem(" - "),
-            new SpinnerItem("Hög APK per krona"),
-            new SpinnerItem("Låg APK per krona"),
+            new SpinnerItem("Hög alkohol per krona"),
+            new SpinnerItem("Låg alkohol per krona"),
             new SpinnerItem("Alkoholfritt")
     };
-
-    };
+}
 
