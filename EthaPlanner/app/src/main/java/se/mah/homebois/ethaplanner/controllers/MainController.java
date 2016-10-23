d96b6ef50cad2fef51ad15491b1dc0275983bc3a
Simon Bothén <caresilabs@gmail.com>
2016-10-23 10:29:57 +0200

    Initial Commit

---
+++ EthaPlanner/app/src/main/java/se/mah/homebois/ethaplanner/controllers/MainController.java
@@ -0,0 +1,8 @@
+package se.mah.homebois.ethaplanner.controllers;
+
+/**
+ * Created by Simon on 10/23/2016.
+ */
+
+public class MainController {
+}
---
+++ EthaPlanner/app/src/main/java/se/mah/homebois/ethaplanner/net/BolagetImageDownloader.java
@@ -0,0 +1,8 @@
+package se.mah.homebois.ethaplanner.net;
+
+/**
+ * Created by Simon on 10/23/2016.
+ */
+
+public class BolagetImageDownloader {
+}
