b8918d1bedf38df187b18f861cc17449d57ab4df
Simon Bothén <caresilabs@gmail.com>
2016-10-23 15:48:42 +0200

    Search activity

--- EthaPlanner/app/src/main/java/se/mah/homebois/ethaplanner/views/SearchActivity.java
+++ EthaPlanner/app/src/main/java/se/mah/homebois/ethaplanner/views/SearchActivity.java
@@ -8,6 +8,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import se.mah.homebois.ethaplanner.R;
@@ -16,9 +17,12 @@ import se.mah.homebois.ethaplanner.views.ListContent.SpinnerItem;
 
 public class SearchActivity extends AppCompatActivity {
 
-    String timeAsText;
-    int time;
-    ArrayAdapter spinnerAdapter;
+    private String       timeAsText;
+    private int          time;
+    private ArrayAdapter spinnerAdapter;
+    private Button       setWeatherButton;
+
+    private Calendar selectedDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +34,16 @@ public class SearchActivity extends AppCompatActivity {
     }
 
     private void RegisterListeners() {
-        Button b1 = (Button) findViewById(R.id.setWeatherButton);
-        b1.setOnClickListener(new ButtonListener());
+        setWeatherButton = (Button) findViewById(R.id.setWeatherButton);
+        setWeatherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDateDialog(v);
+            }
+        });
+
+        selectedDay = Calendar.getInstance();
+        updateSearchDate();
     }
 
     private void SpinnerItems() {
@@ -40,8 +52,7 @@ public class SearchActivity extends AppCompatActivity {
     }
 
 
-    public void showDateDialog(View v)
-    {
+    public void showDateDialog(View v) {
         final View clicked = v;
         Calendar mCurrentDate = Calendar.getInstance();
         int yyyy, mm, dd;
@@ -49,12 +60,10 @@ public class SearchActivity extends AppCompatActivity {
         mm = mCurrentDate.get(Calendar.MONTH);
         dd = mCurrentDate.get(Calendar.DAY_OF_MONTH);
 
-        DatePickerDialog mDatePicker = new DatePickerDialog(v.getContext(), new DatePickerDialog.OnDateSetListener()
-        {
+        DatePickerDialog mDatePicker = new DatePickerDialog(v.getContext(), new DatePickerDialog.OnDateSetListener() {
             @Override
-            public void onDateSet(DatePicker view, int year, int month, int day)
-            {
-                month++;
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+              /*  month++;
                 String m = month+ "";
                 if(month < 10){ m = "0" + m;}
 
@@ -63,7 +72,10 @@ public class SearchActivity extends AppCompatActivity {
 
                 timeAsText = year + "-" + m + "-" + d;
                 //((Button) clicked).setText(timeAsText);
-                time = Integer.parseInt(year + m + d);
+                time = Integer.parseInt(year + m + d);*/
+
+                selectedDay.set(year, month, day, 0, 0);
+                updateSearchDate();
 
             }
         }, yyyy, mm, dd);
@@ -73,10 +85,8 @@ public class SearchActivity extends AppCompatActivity {
 
     }
 
-    private class ButtonListener implements View.OnClickListener {
-        public void onClick(View v)
-        {
-            showDateDialog(v);
-        }
+    private void updateSearchDate() {
+        setWeatherButton.setText(new SimpleDateFormat("dd/MM/yyyy").format(selectedDay));
     }
+
 }
