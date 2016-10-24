f52e5fe57605041621b8b490431779ec1e6b269e
MegaHPEnvy\Patrik <patriknilsson1337@live.se>
2016-10-24 14:48:19 +0200

    Pipeline fix

--- EthaPlanner/app/src/main/java/se/mah/homebois/ethaplanner/controllers/MainController.java
+++ EthaPlanner/app/src/main/java/se/mah/homebois/ethaplanner/controllers/MainController.java
@@ -1,14 +1,15 @@
 package se.mah.homebois.ethaplanner.controllers;
 
-import android.icu.util.Calendar;
-import android.util.Log;
-import android.widget.TextView;
+import java.util.Calendar;
 
+import se.mah.homebois.ethaplanner.Globals;
 import se.mah.homebois.ethaplanner.R;
+import se.mah.homebois.ethaplanner.models.SearchModel;
 import se.mah.homebois.ethaplanner.models.Weather.Forecast;
 import se.mah.homebois.ethaplanner.models.Weather.WeatherModel;
+import se.mah.homebois.ethaplanner.models.WeatherToType;
 import se.mah.homebois.ethaplanner.net.WeatherFetcher;
-import se.mah.homebois.ethaplanner.views.ListContent.ListAdapter;
+import se.mah.homebois.ethaplanner.views.ListContent.SpinnerItem;
 import se.mah.homebois.ethaplanner.views.MainActivity;
 
 /**
@@ -20,6 +21,7 @@ public class MainController {
     private final BolagetController bc;
     private final WeatherController wc;
     private MainActivity activity;
+    private SearchModel searchModel;
 
     private int day;
 
@@ -29,9 +31,16 @@ public class MainController {
         this.wc = wc;
     }
 
-    public void loadResults()
+    public void loadResults(SearchModel searchModel)
     {
-        //activity.getListResult().setAdapter(new ListAdapter(activity, ));
+        this.searchModel = searchModel;
+        long longDate = searchModel.selectedDate;
+
+        long now = Calendar.getInstance().getTimeInMillis();
+        long delta = longDate - now;
+        int day = (int)(delta / Globals.DAYS_IN_MS);
+
+        setWeather(day);
     }
 
     public void setDefaultWeather()
@@ -51,12 +60,27 @@ public class MainController {
     public class WeatherListener implements WeatherFetcher.ModelListener {
         @Override
         public void getWeather(WeatherModel model) {
-
             Forecast cast = model.getQuery().getResults().getChannel().getItem().getForecast()[day];
             activity.setTvDate(cast.getDate());
             activity.setTvHigh(String.format(activity.getResources().getString(R.string.weather_high), cast.getHigh()));
             activity.setTvLow(String.format(activity.getResources().getString(R.string.weather_low), cast.getLow()));
             activity.setTvText(String.format(activity.getResources().getString(R.string.weather_type), cast.getText()));
+
+            //Do seach;
+            String[] types = WeatherToType.getTypes(Integer.parseInt(cast.getCode()));
+            int amountEach = Globals.AMOUNT_IN_RESULT / types.length;
+            int over = amountEach * types.length;
+
+            for (String type: types) {
+                int add = 0;
+                if(over > 0)
+                {
+                    over--;
+                    add = 1;
+                }
+
+                
+            }
         }
     }
 }
