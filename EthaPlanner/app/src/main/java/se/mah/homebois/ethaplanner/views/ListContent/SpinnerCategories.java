e3db2f040469a82f63956dc507f8527a64eabc8f
Tim Lindstam <tim.sunds@aol.com>
2016-10-24 15:08:00 +0200

    Added shit

--- EthaPlanner/app/src/main/java/se/mah/homebois/ethaplanner/views/ListContent/SpinnerCategories.java
+++ EthaPlanner/app/src/main/java/se/mah/homebois/ethaplanner/views/ListContent/SpinnerCategories.java
@@ -11,10 +11,11 @@ public class SpinnerCategories {
     private static Context context;
 
     public static SpinnerItem[] spinnerItems = new SpinnerItem[]{
-            new SpinnerItem(0, context.getResources().getString(R.string.high_alc_per_crown) ),
-            new SpinnerItem(1, context.getResources().getString(R.string.low_alc_per_crown)),
-            new SpinnerItem(2, context.getResources().getString(R.string.alcohol_free)),
-            new SpinnerItem(3, context.getResources().getString(R.string.no_sort_by_apk))
+            new SpinnerItem(0, context.getResources().getString(R.string.no_sort_by_apk)),
+            new SpinnerItem(1, context.getResources().getString(R.string.high_alc_per_crown) ),
+            new SpinnerItem(2, context.getResources().getString(R.string.low_alc_per_crown)),
+            new SpinnerItem(3, context.getResources().getString(R.string.alcohol_free))
+
     };
 
     public static void setContext(Context context) {context = context;}
